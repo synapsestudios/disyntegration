f99447431dbc3e4bad1efc0704b8ff6c7db25d5c
Andrew Reida <andrew@syn0.com>
2014-09-26 15:46:42 -0700

    Updating page functions

--- src/disyntegration.js
+++ src/disyntegration.js
@@ -1,5 +1,8 @@
 function Iframe()
 {
+    var isReady;
+    var oldBody;
+
     this.$el = $('iframe#test-iframe');
 
     if (this.$el.length == 0) {
@@ -7,9 +10,6 @@ function Iframe()
 
         $('body').prepend(this.$el);
     }
-
-    var isReady;
-    var oldBody;
 
     this.$el[0].onload = function() {
         isReady = true;
@@ -25,13 +25,6 @@ function Iframe()
 
     var KeyboardEvent = function(name) {
         var event = document.createEvent('KeyboardEvent');
-        event.initEvent(name, true, false);
-
-        return event;
-    };
-
-    var MouseEvent = function(name) {
-        var event = document.createEvent('MouseEvents');
         event.initEvent(name, true, false);
 
         return event;
@@ -74,21 +67,6 @@ function Iframe()
         return this;
     };
 
-    this.onBodyChange = function(done) {
-        var self = this;
-
-        if (! oldBody) {
-            oldBody = this.getBody();
-        }
-
-        if (oldBody == this.getBody()) {
-            setTimeout(function() { self.onBodyChange(done) }, 200);
-        } else {
-            oldBody = undefined;
-            done();
-        }
-    };
-
     this.ready = function(callback) {
         var self = this;
 
@@ -108,22 +86,45 @@ function Iframe()
         return this;
     };
 
-    this.waitFor = function(condition, callback, maxTimeout) {
-        var maxTimeout, self;
+    // Waits for a condition then executes a callback
+    this.waitFor = function(condition, callback, timeout) {
+        var self = this;
 
-        self = this;
+        if (typeof timeout !== 'number') {
+            timeout = 5000;
+        }
 
-        maxTimeout = maxTimeout || 1000;
-
-        if(maxTimeout <= 0) {
+        if (timeout <= 0) {
             return false;
         }
 
         if(condition()) {
             callback();
         } else {
-            setTimeout(function() { self.waitFor(condition, callback, maxTimeout - 100) }, 100);
+            setTimeout(function() { self.waitFor(condition, callback, timeout - 100) }, 100);
         }
+    };
+
+    // Executes callback as soon as the body changes
+    this.waitForBodyChange = function(callback, timeout) {
+        var self = this;
+
+        this.waitFor(function() {
+            if (! oldBody) {
+                oldBody = self.getBody();
+            }
+
+            return oldBody !== self.getBody();
+        }, callback, timeout);
+    };
+
+    // Executes callback as soon as there is at least one of the selector present
+    this.waitForElement = function(selector, callback, timeout) {
+        var self = this;
+
+        this.waitFor(function() {
+            return self.find(selector).length;
+        }, callback, timeout);
     };
 };
 
